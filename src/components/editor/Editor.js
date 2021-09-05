a2c6d70d805ca759b52f72d60017c3753edc9883
Kim-Eungseo <juniormoo87@gmail.com>
2021-09-06 00:26:23 +0900

    TESTING COMMITS

---
+++ src/components/editor/Editor.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import classnames from 'classnames/bind';
+
+// const cx = classnames.bind()
+
+class EditorFrame extends Component{
+    render() {
+        const {header, editor, preview} = this.props;
+        return(
+            <div className={}
+        )
+
+    }
+}
