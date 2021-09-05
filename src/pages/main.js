a8e8eaaa167ccb1f3c2857e9f6c72d9fe70cc1c5
Jonghan Park <me@han-park.info>
2021-09-06 00:22:06 +0900

    go to sleep

---
+++ src/pages/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header"
+
+
+// import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Main() {
+  return (
+    <div>
+        <Header />
+        This is a main page.
+    </div>
+  );
+}
+
+export default Main;
