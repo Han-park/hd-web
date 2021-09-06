35820a0d2d20461c26c46429de01d4a26a1be429
Jonghan Park <me@han-park.info>
2021-09-06 12:00:04 +0900

    good morning

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,17 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
+    },
+    appBar: {
+        backgroundColor:'#FFF',
+        color: 'black',
+        elevation: (1),
+
     },
     menuButton: {
       marginRight: theme.spacing(2),
@@ -19,22 +25,25 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
+const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
 function Header() {
     const classes = useStyles();
 
     return (
       <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <AppBar position="fixed">
+          <Toolbar className={classes.appBar}>
+            {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"> */}
               {/* <MenuIcon /> */}
-            </IconButton>
+            {/* </IconButton> */}
             <Typography variant="h6" className={classes.title}>
               Higher Definition
             </Typography>
             <Button color="inherit">Login</Button>
           </Toolbar>
         </AppBar>
+        {loremIpsum}{loremIpsum}{loremIpsum}{loremIpsum}{loremIpsum}{loremIpsum}{loremIpsum}{loremIpsum}{loremIpsum}{loremIpsum}
       </div>
     );
 }
