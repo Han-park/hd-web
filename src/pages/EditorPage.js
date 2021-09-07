e85e445e44e86441ba652ef1338e58a176c29111
Kim-Eungseo <juniormoo87@gmail.com>
2021-09-08 04:11:39 +0900

    Editor 틀 완성

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,10 +1,18 @@
 import React from "react";
 import PageTemplate from "../components/common/PageTemplate";
+import EditorTemplate from "../components/editor/EditorTemplate";
+import EditorHeader from '../components/editor/EditorHeader';
+import EditorPane from '../components/editor/EditorPane';
+import PreviewPane from "../components/editor/EditorPage";
 
 const editorPage = () => {
     return(
         <div>
-            <PageTemplate/>
+            <EditorTemplate
+                header={<EditorHeader/>}
+                editor={<EditorPane/>}
+                preview={<PreviewPane/>}
+            />
         </div>
     );
 };
